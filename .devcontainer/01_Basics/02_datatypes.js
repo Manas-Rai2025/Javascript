15fe065bf424f1526e33a149653bccd01c6cf70e
Manas Rai <manasrai95190@gmail.com>
2026-07-31 18:12:31 +0000

    datatypes

---
+++ .devcontainer/01_Basics/02_datatypes.js
@@ -0,0 +1,10 @@
+let Name = "Manas"
+let age = 21
+let isAdult = true
+
+console.log(typeof Name)
+console.log(typeof age)
+console.log(typeof isAdult)
+// Name = String
+// age = number
+// isAdult = boolean
