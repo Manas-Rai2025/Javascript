448d9e35144c438497d5512f32893c093477ae4a
Manas Rai <manasrai95190@gmail.com>
2026-07-31 07:21:11 +0000

    container config done

---
+++ .devcontainer/01_Basics/01_variables.js
@@ -0,0 +1,10 @@
+const accountId = 144553
+let accountEmail = "Manas@google.com"
+let accountPassword = "Manas@123"
+accountCity = "Banglore"
+let accountState = "Karnataka"
+
+
+
+// accountId = 2// this is not allowed because accountId is a constant variable
+console.table([accountId, accountEmail, accountPassword, accountCity, accountState]); 
