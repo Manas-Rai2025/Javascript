ed4b23ff2d0cf5a60928661d7d0c8da95a03cf8f
Manas Rai <manasrai95190@gmail.com>
2026-08-01 18:51:24 +0000

    Configure done

---
+++ 01_Basics/03_Strings.js
@@ -0,0 +1,19 @@
+const Name = "Manas"
+const repoCount = 5
+
+console.log(`Hello my name is ${Name} and I have ${repoCount} repositories.`);
+
+
+const gameName = new String("Chess")
+console.log(gameName[0]);
+console.log(gameName.length);
+
+
+const newString = gameName.concat(" is a fun game")
+console.log(newString);
+
+const  anotherString = gameName.slice(0,5)
+console.log(anotherString);
+
+const newStringOne = "Hitesh"
+console.log(newStringOne.trim());
---
+++ 01_Basics/04_Numbers.js
@@ -0,0 +1,25 @@
+const score = 100
+console.log(score);
+const balance = new Number(100)
+console.log(balance);
+
+console.log(balance.toString().length);
+console.log(balance.toFixed(2));
+
+const anothernumber = 23.8966
+console.log(anothernumber.toPrecision(3));
+
+const hundreds = 1000000
+console.log(hundreds.toLocaleString("en-IN"));
+
+
+
+
+
+
+
+
+
+
+
+
